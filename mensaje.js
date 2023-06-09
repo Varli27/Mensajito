8f2b6d59c742fab0f9873878e49025634c3c7b65
Varli27 <136032282+Varli27@users.noreply.github.com>
2023-06-08 22:27:13 -0300

    Add files via upload

---
+++ mensaje.js
@@ -0,0 +1,23 @@
+let mensaje = alert (' Hola Amorcito ❤ ')
+let mensaje2 = alert (' Si si vos cosita linda')
+let mensaje3 = alert (' Te amo mucho :3')
+let mensaje4 = alert (' No soy infantil (dice la que puso un ":3")')
+let mensaje5 = alert (' volviendo al tema ')
+let mensaje6 = alert ('vos me amas?  ')
+let mensaje7 = prompt ('ingrese su respuesta a la pregunta anterior (y que sea positiva🔪 (? )')
+let mensaje8 = alert (' buenu ya que respondiste emm tengo que agregar otra cosa')
+let mensaje9 = alert (' 1) nu me engañes 😔')
+let mensaje10= alert (' 2) extraño mi cana jeje')
+let mensaje11= alert ('ahora chi')
+let mensaje12= alert ('Que tengas un hermoso día mi vida hermoso, y que sepas que para todo lo que necesites y quieras ahi voy a estar ❤')
+let mensaje13 = alert ('Te amo ❤')
+
+
+
+
+
+
+
+
+
+
